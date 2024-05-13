a36f35adfb03222d90c85b57e15cbe0f784c1b00
rlacodud <rlacodud0313@naver.com>
2024-05-13 23:00:41 +0900

    feat: 바텀시트 1차 업로드

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,18 @@
 // (예: 좌측에 있을 때는 진행률에 따라 좌측으로 화살표가 이동되어야 할 수 있음)
 // Tooltip의 화살표는 항상 Tooltip 영역 범위 안에 있어야 하며 좌/우측 최소 6px의 마진이 있어야 함
 // Tooltip은 Progress bar의 영역을 벗어날 수 없음
-import { ref, reactive, onMounted, onUpdated, computed } from 'vue';
 
-const tooltip = ref(null);
-const arrow = ref(null);
-const currentbar = ref(null);
-const progressbar = ref(null);
-const minMargin = 6;
-let tooltipLeft = 0;
-let arrowLeft = 0;
+import { ref, reactive } from 'vue';
+import BottomSheet from '../../components/common/BottomSheet.vue';
+
+const customPriceValue = ref(null);
+const isOpenBottomsheet = ref(false);
+const showBottomsheet = () => {
+  isOpenBottomsheet.value = true;
+}
+const closeBottomsheet = () => {
+  isOpenBottomsheet.value = false;
+}
 
 const prices = reactive([
   { price: 0, isActive: true },
@@ -24,45 +27,16 @@ const prices = reactive([
   { price: 95000, isActive: false },
   { price: 100000, isActive: false },
 ]);
+
 const maxPrice = Math.max(...prices.map(price => price.price));
 let activePrice = ref(prices.find(price => price.isActive ? price.isActive : prices[0]).price);
 
 const progressPercentage = computed(() => {
-  return `${activePrice.value / maxPrice * 100}%`;
-});
-
-const handleTooltipPosition = (progressbarWidth) => {
-  const currentbarWidth = currentbar.value.clientWidth;
-  const tooltipWidth = tooltip.value.clientWidth / 2;
-  const arrowWidth = arrow.value.offsetWidth / 2;
-  // 좌측을 넘을 때
-  if (currentbarWidth < tooltipWidth) {
-    tooltipLeft = -currentbarWidth;
-    arrowLeft = minMargin < currentbarWidth
-    ? currentbarWidth - arrowWidth
-    : minMargin;
-  }
-  // 우측을 넘을 때
-  else if (progressbarWidth < currentbarWidth + tooltipWidth) {
-    tooltipLeft = progressbarWidth - (currentbarWidth + tooltipWidth * 2);
-    arrowLeft = minMargin < progressbarWidth - currentbarWidth
-    ? progressbarWidth - currentbarWidth + arrowWidth
-    : (tooltipWidth * 2) - (arrowWidth * 2) - minMargin;
-  }
-  else {
-    tooltipLeft = -tooltipWidth;
-    arrowLeft = tooltipWidth - arrowWidth;
-  }
-  tooltip.value.style.transform = `translateX(${tooltipLeft}px)`;
-  arrow.value.style.transform = `translateX(${arrowLeft}px)`;
-}
-onMounted(() => {
-  const progressbarWidth = progressbar.value.clientWidth;
-  handleTooltipPosition(progressbarWidth);
-});
-onUpdated(() => {
-  const progressbarWidth = progressbar.value.clientWidth;
-  handleTooltipPosition(progressbarWidth);
+  if (activePrice.value > maxPrice) {
+    return '100%'
+  } else {
+    return `${activePrice.value / maxPrice * 100}%`
+  };
 });
 
 const handleButtonChange = (index) => {
@@ -70,6 +44,12 @@ const handleButtonChange = (index) => {
     button.isActive = idx === index;
   });
   activePrice.value = prices[index].price;
+  showBottomsheet();
+};
+
+const handleChangecustomPrice = () => {
+  activePrice.value = customPriceValue.value;
+  showBottomsheet();
 };
 
 </script>
@@ -83,79 +63,66 @@ const handleButtonChange = (index) => {
         </li>
       </ul>
     </div>
-    <div class="progress-bar_container">
-      <div class="progress-bar" ref="progressbar">
-        <div class="current-bar" ref="currentbar" :style="{ '--progress-width': progressPercentage }">
-          <span class="tooltip_container" ref="tooltip">
-            {{ activePrice.toLocaleString() }}
-            <span class="arrow" ref="arrow"></span>
-          </span>
-        </div>
-      </div>
+    <div class="input-container">
+      <label for="customPrice">커스텀 가격</label>
+      <input v-model="customPriceValue" id="customPrice" type="number" />
+      <button type="button" @click="handleChangecustomPrice">확인</button>
     </div>
   </main>
+  <BottomSheet
+    :maxPrice="maxPrice"
+    :customPriceValue ="customPriceValue"
+    :progressPercentage="progressPercentage"
+    :activePrice="activePrice"
+    :isOpenBottomsheet="isOpenBottomsheet"
+    @closeBottomsheet="closeBottomsheet"
+  />
 </template>
 
 <style scoped lang="scss">
 .contents {
   padding: 20px;
   .button_container {
-  ul {
-    display: flex;
-    gap: 10px;
-    list-style: none;
-  }
-  button {
-    padding: 4px 6px;
-    color: black;
-    border: 1px solid black;
-    outline: none;
-    border-radius: 10px;
-    background-color: white;
-    &.active {
-      color: white;
-      border: none;
-      background-color: black;
+    ul {
+      display: flex;
+      gap: 10px;
+      list-style: none;
     }
-  }
-  & + .progress-bar_container {
-    margin-top: 20px;
-  }
-}
-.progress-bar {
-  position: relative;
-  width: 100%;
-  height: 10px;
-  border-radius: 10px;
-  background-color: #d8d8d8;
-  .current-bar {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: var(--progress-width);
-    height: 100%;
-    border-radius: 10px;
-    background-color: #0077ff;
-    .tooltip_container {
-      position: absolute;
-      top: calc(100% + 10px);
-      left: 100%;
-      padding: 10px 16px;
-      color: white;
-      border-radius: 4px;
-      background-color: black;
-      .arrow {
-        display: inline-block;
-        position: absolute;
-        left: 0;
-        bottom: 100%;
-        border-top: 3px solid transparent;
-        border-left: 4px solid transparent;
-        border-right: 4px solid transparent;
-        border-bottom: 6px solid black;
+    button {
+      padding: 4px 6px;
+      color: black;
+      border: 1px solid black;
+      outline: none;
+      border-radius: 10px;
+      background-color: white;
+      &.active {
+        color: white;
+        border: none;
+        background-color: black;
       }
     }
   }
-}
+  .input-container {
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+    label {
+      font-size: 14px;
+      font-weight: 700;
+    }
+    input {
+      margin-left: 10px;
+      padding: 4px 6px;
+      border-radius: 10px;
+    }
+    button {
+      margin-left: 10px;
+      padding: 4px 6px;
+      color: white;
+      outline: none;
+      border-radius: 10px;
+      background-color: black;
+    }
+  }
 }
 </style>
