c1c721b3b6473d24eb320028930c9a1d0e5de7ed
sxul-i <77483595+sxul-i@users.noreply.github.com>
2024-05-15 18:11:15 +0900

    Update index.vue
    툴팁 화살표 마무리 작업 업뎃

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,122 @@
 <template>
 	<div class="container">
 		<div ref="boxRef" class="range_box">
-			<input v-model="sliderValue" type="range" @input="updateSlider" />
+			<input v-model="sliderValue" type="range" readonly />
 			<div ref="tipRef" class="tooltip" :style="{ left: inputValue }">
-				<p>{{ sliderValue }}%</p>
-				<span class="arrow" :style="{ left: arrowValue ? arrowValue : '50%' }"></span>
+				<p>{{ commas(sliderValue * 1000) }}원</p>
+				<span class="arrow" :style="{ left: arrowValue === null ? '50%' : arrowValue }"></span>
 			</div>
+		</div>
+		<div class="btn">
+			<button @click="updateValue(0)">0원</button>
+			<button @click="updateValue(1000)">1,000원</button>
+			<button @click="updateValue(2000)">2,000원</button>
+			<button @click="updateValue(3000)">3,000원</button>
+			<button @click="updateValue(5000)">5,000원</button>
+			<button @click="updateValue(10000)">10,000원</button>
+			<button @click="updateValue(50000)">50,000원</button>
+			<button @click="updateValue(90000)">90,000원</button>
+			<button @click="updateValue(95000)">95,000원</button>
+			<button @click="updateValue(100000)">100,000원</button>
 		</div>
 	</div>
 </template>
 
 <script>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted, onBeforeUnmount, nextTick } from 'vue';
 
 export default {
 	setup() {
-		const sliderValue = ref(0);
-		const boxRef = ref(0);
+		const maxAmount = 100000; // 최대 금액
+		const sliderValue = ref(0); // input 값
+		const boxRef = ref(null);
 		const boxWidth = ref(0);
-		const tipRef = ref(0);
+		const tipRef = ref(null);
 		const tipWidth = ref(0);
 
-		function updateSlider(event) {
-			sliderValue.value = event.target.value;
+		//숫자 3자리수 콤마 코드
+		function commas(x) {
+			try {
+				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+			} catch (e) {
+				return '0';
+			}
 		}
+		// 요소넓이 계산
 		function measureMaxWidth() {
-			if (boxRef.value) {
-				boxWidth.value = boxRef.value.offsetWidth;
-			}
-			if (tipRef.value) {
-				tipWidth.value = tipRef.value.offsetWidth;
-			}
+			nextTick(() => {
+				if (boxRef.value) {
+					boxWidth.value = boxRef.value.offsetWidth;
+				}
+				if (tipRef.value) {
+					tipWidth.value = tipRef.value.offsetWidth;
+				}
+			});
 		}
-		onMounted(measureMaxWidth);
-		// 퍼센트
+		// 금액 백분율 계산
+		const sliderValuePercentage = e => {
+			return (e / maxAmount) * 100;
+		};
+		// 금액변경 버튼
+		function updateValue(value) {
+			sliderValue.value = sliderValuePercentage(value);
+			measureMaxWidth();
+		}
+		// 툴팁 이동값 계산
 		const inputValue = computed(() => {
 			const valueWidth = (sliderValue.value / 100) * boxWidth.value;
 			const tipHalf = tipWidth.value / 2;
 			const result =
 				valueWidth > boxWidth.value - tipHalf
-					? boxWidth.value
+					? `${boxWidth.value - tipWidth.value}px`
 					: `${valueWidth - tipHalf <= 0 ? 0 : valueWidth - tipHalf}px`;
 			return result;
 		});
-		// 화살표
+		// 툴팁 화살표 이동값 계산
 		const arrowValue = computed(() => {
+			// 툴팁절반 tipHalf
 			const valueWidth = (sliderValue.value / 100) * boxWidth.value;
 			const tipHalf = tipWidth.value / 2;
 			let result = null;
+			// 요건의 6px + 화살표 넓이 6px = 12px
 			if (valueWidth - tipHalf <= 0) {
 				console.log('왼쪽끝');
-				result = valueWidth - tipHalf < -6 ? 'calc(50% - 6px)' : `calc(50% - ${valueWidth - tipHalf})`;
+				result =
+					tipHalf - 12 < tipHalf - valueWidth
+						? `calc(50% - ${Math.floor(tipHalf - 12)}px)`
+						: `calc(50% - ${Math.floor(tipHalf - valueWidth)}px)`;
 			} else if (valueWidth > boxWidth.value - tipHalf) {
 				console.log('오른쪽끝');
 				result =
-					boxWidth.value - tipHalf - valueWidth < -6
-						? 'calc(50% + 6px)'
-						: `calc(50% + ${boxWidth.value - tipHalf - valueWidth})`;
+					Math.abs(boxWidth.value - tipHalf - valueWidth) < tipHalf - 12
+						? `calc(50% + ${Math.floor(Math.abs(boxWidth.value - tipHalf - valueWidth))}px)`
+						: `calc(50% + ${Math.floor(tipHalf - 12)}px)`;
+			} else {
+				result = null;
 			}
 			return result;
 		});
 
+		onMounted(() => {
+			measureMaxWidth();
+			window.addEventListener('resize', measureMaxWidth);
+		});
+
+		onBeforeUnmount(() => {
+			window.removeEventListener('resize', measureMaxWidth);
+		});
+
 		return {
 			sliderValue,
-			updateSlider,
+			updateValue,
+			commas,
 			inputValue,
 			arrowValue,
 			boxRef,
 			boxWidth,
 			tipRef,
 			tipWidth,
+			sliderValuePercentage,
 		};
 	},
 };
@@ -82,18 +129,20 @@ export default {
 }
 .range_box {
 	position: relative;
-	width: 500px;
+	width: 100%;
 	padding-top: 50px;
-	margin: 0 auto;
+	margin: 0 auto 20px;
 }
 input[type='range'] {
 	display: block;
 	width: 100%;
 	height: 15px;
+	margin: 0;
 	overflow: hidden;
-	-webkit-appearance: none;
 	background-color: #dfdfdf;
 	border-radius: 10px;
+	pointer-events: none;
+	-webkit-appearance: none;
 }
 
 input[type='range']::-webkit-slider-runnable-track {
@@ -108,33 +157,51 @@ input[type='range']::-webkit-slider-thumb {
 	height: 10px;
 	cursor: ew-resize;
 	background: #097cff;
-	box-shadow: -500px 0 0 500px #097cff;
+	box-shadow: -100vw 0 0 100vw #097cff;
 	-webkit-appearance: none;
 }
 .tooltip {
 	position: absolute;
-	top: 25px;
+	top: 22px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	width: 36px;
-	height: 17px;
+	margin: 0;
+}
+.tooltip p {
+	padding: 2px 16px;
+	min-width: 18px;
+	font-size: 13px;
 	background: #333;
 	color: #fff;
 	border-radius: 5px;
+	white-space: nowrap;
+	margin: 0;
 }
 .arrow {
 	display: block;
 	position: absolute;
-	top: 17px;
+	top: 19px;
 	left: 50%;
 	transform: translateX(-50%);
-	border-top: 5px solid #333;
-	border-left: 5px solid transparent;
-	border-right: 5px solid transparent;
+	border-top: 6px solid #333;
+	border-left: 6px solid transparent;
+	border-right: 6px solid transparent;
 	border-bottom: 0px solid transparent;
 }
-.tooltip p {
-	font-size: 11px;
+.btn {
+	display: flex;
+	gap: 10px;
+	flex-wrap: wrap;
+}
+.btn button {
+	background: #efefef;
+	border: 1px solid #dfdfdf;
+	padding: 3px 10px;
+	color: #4b4b4b;
+	border-radius: 5px;
+}
+.btn button:active {
+	background: #d5d5d5;
 }
 </style>
