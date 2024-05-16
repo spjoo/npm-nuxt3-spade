4d68def7432a2ac19292f2201b3ba26e394a524e
spjoo <joo@spadecompany.kr>
2024-05-17 08:04:50 +0900

    feat: progressbar 적용

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,147 @@
-<script setup></script>
+<script setup>
+import { ref, computed, nextTick, onMounted } from 'vue';
 
-<template></template>
+const priceList = [0, 1000, 2000, 5000, 9000, 10000];
 
-<style scoped></style>
+const currentPrice = ref(priceList[0]);
+const barEl = ref(null);
+const tipEl = ref(null);
+const setPrice = val => {
+	currentPrice.value = val;
+	if (tipEl.value) calcPos();
+};
+const percentageVal = computed(() => {
+	return (currentPrice.value / 10000) * 100;
+});
+const tipPos = ref(null);
+const arrowPos = ref(null);
+const progressWidth = ref(0);
+const setProgressWidth = () => {
+	if (!barEl.value) {
+		nextTick(() => {
+			progressWidth.value = barEl.value.offsetWidth;
+		});
+	} else {
+		progressWidth.value = barEl.value.offsetWidth;
+	}
+};
+const calcPos = async () => {
+	let tipWidth = 0;
+	await nextTick(() => {
+		tipWidth = tipEl.value.offsetWidth;
+	});
+	const currentProgressWidth = (percentageVal.value / 100) * progressWidth.value;
+	// 초기화
+	tipPos.value = 0;
+	arrowPos.value = 0;
+	// 왼쪽
+	if (currentProgressWidth < tipWidth / 2) {
+		tipPos.value = currentProgressWidth - tipWidth / 2;
+		arrowPos.value = tipPos.value < 4 + 6 ? tipPos.value + 4 + 6 : tipPos.value;
+	}
+	// 오르쪽
+	else if (progressWidth.value - currentProgressWidth < tipWidth / 2) {
+		tipPos.value = tipWidth / 2 - (progressWidth.value - currentProgressWidth);
+		arrowPos.value = tipPos.value > 4 + 6 ? tipPos.value - 4 - 6 : tipPos.value;
+	}
+};
+
+onMounted(async () => {
+	await setProgressWidth();
+	await calcPos();
+});
+</script>
+
+<template>
+	<main>
+		<ul class="price_area">
+			<li v-for="(price, i) in priceList" :key="i">
+				<button @click="setPrice(price)">{{ price.toLocaleString() }}</button>
+			</li>
+		</ul>
+		<section>
+			<div class="progress_area">
+				<progress ref="barEl" max="100" :value="percentageVal"></progress>
+				<p
+					ref="tipEl"
+					class="tooltip"
+					:class="{ is_on: arrowPos !== null && tipPos !== null }"
+					:style="[{ left: `${percentageVal}%` }, { '--tipPos': tipPos }, { '--arrowPos': arrowPos }]"
+				>
+					{{ currentPrice.toLocaleString() }}원
+				</p>
+				<p>{{ priceList[priceList.length - 1].toLocaleString() }}원</p>
+			</div>
+		</section>
+	</main>
+</template>
+
+<style scoped>
+.price_area {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 16px;
+	list-style: none;
+}
+.price_area button {
+	min-width: 100px;
+	padding: 4px 8px;
+	color: #555;
+	border: 1px solid #ccc;
+	background: #f0f0f0;
+}
+.progress_area {
+	position: relative;
+	margin-top: 50px;
+}
+.progress_area progress {
+	width: 100%;
+	height: 16px;
+	appearance: none;
+}
+.progress_area progress::-webkit-progress-bar {
+	overflow: hidden;
+	background: #f0f0f0;
+	border: 0 none;
+	border-radius: 12px;
+}
+.progress_area progress::-webkit-progress-value {
+	background: #43f15e;
+	border-radius: 12px;
+}
+.progress_area .tooltip {
+	display: flex;
+	position: absolute;
+	bottom: 100%;
+	align-items: center;
+	justify-content: center;
+	margin-top: 0;
+	margin-bottom: 10px;
+	margin-left: calc(var(--tipPos) * -1px);
+	padding: 6px 8px;
+	background: #000;
+	border-radius: 4px;
+	color: #fff;
+	font-size: 13px;
+	line-height: 1;
+	white-space: nowrap;
+	opacity: 0;
+	transform: translateX(-50%);
+}
+.progress_area .tooltip.is_on {
+	opacity: 1;
+	transition: opacity 0.3s;
+}
+.progress_area .tooltip::before {
+	display: block;
+	content: '';
+	position: absolute;
+	top: 100%;
+	left: 50%;
+	margin-left: -4px;
+	border-top: 6px solid #000;
+	border-left: 4px solid transparent;
+	border-right: 4px solid transparent;
+	transform: translateX(calc(var(--arrowPos) * 1px));
+}
+</style>
